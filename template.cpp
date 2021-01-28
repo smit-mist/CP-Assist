0fc59922f3196b8ca554b26f1f21b334668b8ea5
smit-mist <smit3355mistry@gmail.com>
2021-01-28 14:20:16 +0530

    TEMPLATE updated

--- template.cpp
+++ template.cpp
@@ -1,51 +1,50 @@
 //Classic
+//g++ -std=c++11 -O2 -Wall a.cpp -o test
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define ld long double
 #define vll vector<ll>
 #define vi vector<int>
 #define pi pair<int,int>
 #define vp vector<pi>
 #define pb push_back
 #define mp make_pair
+#define mt make_tuple
 #define F first
 #define S second
 #define For(i,a,b) for(ll i=a;i<b;i++)
 #define endl "\n"
+#define debug2(x,y) cout<<"AA Baju Smit----> "<<#x<<" -> "<<x<<" | "<<#y<<" -> "<<y<<endl;
 #define debug(x) cout<<"AA Baju Smit------> "<<#x<<" -> "<<x<<endl
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define mint map<int,int>
-#define mod 998244353
+#define mod 1000000007
 #define ciN cin
-#define Z(s) get<0>(s)
-#define O(s) get<1>(s)
-#define T(s) get<2>(s)
+#define tu(a,s) get<a>(s)
+#define tin tuple<int,int,int>
 ////////////////////////////////////////////////////////////////
-
 
 void jabru() {
+
 }
-////////////////////////////////////////////////////////////////
 
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int t;
+
+	ll t;
 	t = 1;
 	cin >> t;
 	while (t--) {
 		jabru();
 	}
 
-
 	return 0;
+
+
+
+
 }
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////
-//Kai baju bhai?s
