3b6c5d7d7c280da53831fc5f8e0bbc38ce400f4e
smit-mist <smit3355mistry@gmail.com>
2021-05-07 11:45:51 +0530

    kth-order statistics added using BIT

--- range_queries/kth_order_stat_using_bit.cpp
+++ range_queries/kth_order_stat_using_bit.cpp
@@ -1,8 +1,5 @@
 //Classic
-//g++ -std=c++17 -O2 -Wall a.cpp -o test
-// #pragma GCC optimize("Ofast")
-// #pragma GCC target("avx,avx2,fma")
-// #pragma GCC optimization ("unroll-loops")
+// //g++ -std=c++17 -O2 -Wall a.cpp -o test
 // _()_
 #include<bits/stdc++.h>
 using namespace std;
@@ -21,11 +18,9 @@ using namespace std;
 #define F first
 #define S second
 #define For(i,a,b) for(ll i=a;i<b;i++)
-#define endl "\n"
+//#define endl "\n"
 #define debug2(x,y) cout<<"This side ----> "<<#x<<" -> "<<x<<" | "<<#y<<" -> "<<y<<endl;
 #define debug(x) cout<<"This side    ----> "<<#x<<" -> "<<x<<endl
-//#define ai(a, n) for (int ele = 0; ele < n; ele++) cin >> a[ele];
-//#define ao(a, n) {for (int ele = 0; ele < (n); ele++) {cout << a[ele]<<" "; } cout << '\n';}
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define mint map<int,int>
@@ -36,64 +31,54 @@ using namespace std;
 #define ter(x,y,z) ((x)?y:z)
 /////////////
 bool isGoogles = 0;
-const ll maxn = 1e5 + 10;
+const ll maxn = 2e3 + 3;
 const ll max_val = 2e5 + 10;
-const ll mod = 1000000007;
-const ll bits = 19;
+ll mod = 1000000007;
+const ll bits = 20;
 ll caseNumber = 1;
-/*
-
-*/
 ////////////////////////////////////////////////////////////////
-void solve() {
-	int n; cin >> n;
-	vi v(n);
-	int dig[10][2];
-	for (int i = 0; i < 10; i++) {
-		dig[i][0] = 0; dig[i][1] = 0;
+vll bst(maxn, 0);
+vi adj[maxn];
+vi ans(maxn, 0);
+vi small(maxn, 0);
+ll func(ll k) {
+	ll s = 0;
+	while (k >= 1 && k < maxn) {
+		s += bst[k];
+		k -= (k & (-k));
 	}
+	return s;
+}
+ll give_kth(int k)
+{
 
-	For(i, 0, n) {
-		int x; cin >> x;
-		int mx = v[i] % 10, mn = v[i] % 10;
-		int tem = v[i];
-		while (tem > 0) {
-			mx = max(mx, v[i] % 10); mn = min(mn, v[i] % 10);
-		}
-		int s = mx * 11 + mn * 7;
-		s %= 100;
-		int cnt = 0;
-		int last = 0;
-		while (s > 0) {
-			last = s % 10;
-			s /= 10;
-			cnt++;
-		}
-		if (cnt == 1 || cnt == 0) {
-			if (i % 2) {
-				dig[0][1]++;
-			}
-			else
-				dig[0][0]++;
-		}
-		else {
-			if (i % 2) {
-				dig[last][1]++;
-			}
-			else
-				dig[last][0]++;
-		}
+	int l = 0;
+	int h = bst.size();
+	while (l < h)
+	{
+		int mid = (l + h) / 2;
+		if (k <= func(mid))
+			h = mid;
+		else
+			l = mid + 1;
+	}
+	if (func(l) < k)return -1;
+	return l;
+}
+void add(ll k, ll x) {
+	while (k <= maxn - 1) {
+		bst[k] += x;
+		k += (k & (-k));
+	}
+}
 
-	}
-	ll ans = 0;
-	for (int i = 0; i < 10; i++) {
-		int aa = (dig[i][0] * (dig[i][0] - 1)) / 2;
-		int bb = (dig[i][1] * (dig[i][1] - 1)) / 2;
-		ans += min(2, aa + bb);
-	}
-	cout << ans << endl;
+void jabru() {
+	// finding the kth smallest number present in DS in logn*logn
+	// deletion, insertion, searching, kthsmallest operation alllowed
+
 
 }
+
 
 
 bool TestCase = 0;
@@ -107,14 +92,8 @@ bool TestCase = 0;
 
 
 
-void brain_wash() {
-
-}
 
 
-/*
-
-*/
 //////////////////////////////////////////////////////////
 
 int main() {
@@ -131,18 +110,7 @@ int main() {
 	while (t--) {
 		if (isGoogles) {cout << "Case #" << caseNumber << ": ";} caseNumber++;
 		jabru();
-		brain_wash();
 
 	}
 	return 0;
 }
-
-/*
-Sunday - Graph
-Monday - Graph
-Tuesday - Math
-Wednesday - DP
-Thursday - DP/RQ
-Friday - RQ
-Saturday - Tree
-*/
